52f8b752c23be38fc5d5ef64632495cc16623284
Rafael Rosa - Sidedoor { } <rafael@sidedoor.com.br>
2020-12-09 22:33:19 -0300

    scheema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
-<<<<<<< HEAD
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-=======
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
->>>>>>> development
 end
