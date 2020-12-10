61824ed10314e22569e0e7120fa56201c0c6ace5
ANDRE DE SOUZA ALVES <alves.andre.ds@gmail.com>
2020-12-09 23:07:48 -0300

    Revert "add devise User"

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  enum kind: {patitent: 1, health_professional: 2, admin: 3}
-  has_one_attached :avatar
-end
