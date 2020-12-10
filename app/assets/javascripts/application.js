ae3aedd556ac009598c5beebce793415e8e81f77
Hudson Maradona <hudsonmaradona@gmail.com>
2020-12-09 21:30:35 -0300

    update javascript import

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,9 @@
-// require jquery
-//= require jquery3.5.1
-// require jquery_ujs
-//= require jquery.easing
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.bundle
 //= require sb-admin-2
 //= require chart
-//= require chart-area-demo
+//= require chart-area-demo 
 //= require chart-pie-demo
-
-// require bootstrap_sb_admin_base_v2
+// require turbolinks
 // require_tree .
