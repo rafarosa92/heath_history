779e3e141962976188297957c7ea3bf92686fd80
mp <mploureno@gmail.com>
2020-12-10 23:07:35 -0300

    edit user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.name }
+    avatar { Faker::Avatar.image(slug: "my-own-slug") }
+    email { Faker::Internet.email }
+    password { "123456" }
+    password_confirmation { "123456" }
+    kind { :patient }
+    birthdate { Faker::Date.birthday }
   end
 end
