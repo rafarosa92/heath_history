09ec668b997741f99194c176eb46bcfb57fef573
Valter Fernandes Filho <valterfernandes.dev@gmail.com>
2020-12-10 21:48:56 -0300

    Add user references to model

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,4 +1,5 @@
 class Exam < ApplicationRecord
   belongs_to :appointment
+  belongs_to :user
   has_many_attach :documents
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,5 +1,6 @@
 class Treatment < ApplicationRecord
   belongs_to :appointment
+  belongs_to :user
   has_one_attach :document
 
   enum kind: { medicine: 1, physiotherapy: 2, other: 3 }
