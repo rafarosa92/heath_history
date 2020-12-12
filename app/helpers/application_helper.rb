125d28fa28315692f8c6a65b2cee95009b6a171b
Hudson Maradona <hudsonmaradona@gmail.com>
2020-12-12 16:46:17 -0300

    update images background and add name in form devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,12 @@ module ApplicationHelper
   include ActiveLinkToHelper
   include TemplateNavHelper
 
-  # def current_page(page)
-	 #  case
-	 #    when post.comments.count > 4
-	 #     image_tag "fresh.png", size: "28x28"
-	 #     "5+ comments"
-	 #    when post.comments.count > 2
-	 #     image_tag "rotten.jpeg", size: "31x31"
-	 #     "3+ comments"
-	 #    else
-	 #  end
-  # end
+  def current_page(page)
+	  case page
+	    when 'sessions' then "bg-gradient-primary"
+    	when 'registrations' then "bg-gradient-primary"
+    	when 'passwords' then "bg-gradient-primary"	
+	  end
+  end
 
 end
