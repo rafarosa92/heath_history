38be41fcef530bf08229b5fb230a2d16ff90bcfb
Rafael Rosa - Sidedoor { } <rafael@sidedoor.com.br>
2020-12-12 13:38:35 -0300

    complete fixtures

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,3 @@
 //= require jquery3
 //= require jquery_ujs
-//= require jquery.easing
-//= require bootstrap
-//= require sb-admin-2
-//= require chart
-//= require chart-area-demo
-//= require chart-pie-demo
-//= data-table
-// require_tree .
+//= require_tree .
