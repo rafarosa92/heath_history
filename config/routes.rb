f3129b2d9618a47b4bb931ed4e89af7b345dfae3
Hudson Maradona <hudsonmaradona@gmail.com>
2020-12-12 21:51:02 -0300

    devise update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :users
 
   root to: 'home#index'
@@ -8,4 +9,6 @@ Rails.application.routes.draw do
   resources :treatments
   resources :shared_histories
   resources :exams
+  resources :users
+  
 end
