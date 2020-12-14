2b4982d6268d32a21d3d1db914018fcc69009841
julia <agramartinsj@gmail.com>
2020-12-14 17:15:23 -0300

    dashboard cards and search updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @appointments = Appointment.where(patient_user_id:current_user.id).order(date: :desc).limit(3)
+    @exams = Exam.where(user:current_user.id).order(date: :desc).limit(3)
+    @treatments = Treatment.where(user:current_user.id).order(date: :desc).limit(3)
   end
 end
