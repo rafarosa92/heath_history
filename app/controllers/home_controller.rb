c875135eebf753050b0a903eec86fc91758016a5
julia <agramartinsj@gmail.com>
2020-12-14 20:02:15 -0300

    added show method to search table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,11 @@ class HomeController < ApplicationController
     @exams = Exam.where(user:current_user.id).order(date: :desc).limit(3)
     @treatments = Treatment.where(user:current_user.id).order(date: :desc).limit(3)
  
+    @all = []
+    Appointment.all.map { |a| @all << { 'tipo' => 'Consulta', 'data' => a.date.try(:strftime, '%d/%m/%Y %H:%M'), 'titulo' => a.title, 'link' => "appointments/#{a.id}" } }
+    Exam.all.map { |e| @all << { 'tipo' => 'Exame', 'data' => e.date.try(:strftime, '%d/%m/%Y %H:%M'), 'titulo' => e.name, 'link' => "exams/#{e.id}" } }
+    Treatment.all.map { |t| @all << { 'tipo' => 'Tratamento', 'data' => t.date.try(:strftime, '%d/%m/%Y %H:%M'), 'titulo' => t.title, 'link' => "treatments/#{t.id}" } }
+
   end
 
 
