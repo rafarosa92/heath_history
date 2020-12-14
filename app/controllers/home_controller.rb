63c642ac3abb749c0db9e4873aa38e7ed7326081
julia <agramartinsj@gmail.com>
2020-12-14 19:41:10 -0300

    small fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,9 @@ class HomeController < ApplicationController
     @appointments = Appointment.where(patient_user_id:current_user.id).order(date: :desc).limit(3)
     @exams = Exam.where(user:current_user.id).order(date: :desc).limit(3)
     @treatments = Treatment.where(user:current_user.id).order(date: :desc).limit(3)
+ 
   end
+
+
+
 end
