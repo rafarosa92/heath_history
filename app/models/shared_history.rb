e9651bcfc2192be8171c48931e0430ebce3f99de
Valter Fernandes Filho <valterfernandes.dev@gmail.com>
2020-12-17 11:59:33 -0300

    Add shared history

--- app/models/shared_history.rb
+++ app/models/shared_history.rb
@@ -3,4 +3,6 @@ class SharedHistory < ApplicationRecord
 
   belongs_to :owner_user, foreign_key: 'owner_user_id', class_name: 'User'
   belongs_to :shared_user, foreign_key: 'shared_user_id', class_name: 'User'
+
+  validates :expiration_date, :shared_user_id, :owner_user_id, :appointment_id, presence: true
 end
